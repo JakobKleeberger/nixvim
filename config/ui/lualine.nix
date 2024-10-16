0b9058304208631ba2dbf48e4bf05f6d3b018c2f
confusedalex <alex@confusedalex.dev>
2024-10-16 21:53:16 +0200

    feat: better lualine

--- config/ui/lualine.nix
+++ config/ui/lualine.nix
@@ -2,20 +2,59 @@
   plugins.lualine = {
     enable = true;
     settings = {
-      options = {
-        globalstatus = true;
-        extensions = [
-          "fzf"
-          "neo-tree"
+      alwaysDivideMiddle = true;
+      globalstatus = true;
+      ignoreFocus = [ "neo-tree" ];
+      extensions = [ "fzf" ];
+      theme = "auto";
+      componentSeparators = {
+        left = "|";
+        right = "|";
+      };
+      sectionSeparators = {
+        left = "█"; # 
+        right = "█"; # 
+      };
+      sections = {
+        lualine_a = [ "mode" ];
+        lualine_b = [
+          "branch"
+          ""
+          "diff"
+          "diagnostics"
         ];
-        disabledFiletypes = {
-          statusline = [
-            "startup"
-            "alpha"
-            "ministarter"
-          ];
+        lualine_c = [ "filename" ];
+        lualine_x = [ "filetype" ];
+        lualine_y = [ "progress" ];
+        settings = {
+          alwaysDivideMiddle = true;
+          globalstatus = true;
+          ignoreFocus = [ "neo-tree" ];
+          extensions = [ "fzf" ];
+          theme = "auto";
+          componentSeparators = {
+            left = "|";
+            right = "|";
+          };
+          sectionSeparators = {
+            left = "█"; # 
+            right = "█"; # 
+          };
+          sections = {
+            lualine_a = [ "mode" ];
+            lualine_b = [
+              "branch"
+              ""
+              "diff"
+              "diagnostics"
+            ];
+            lualine_c = [ "filename" ];
+            lualine_x = [ "filetype" ];
+            lualine_y = [ "progress" ];
+            lualine_z = [ ''" " .. os.date("%R")'' ];
+          };
         };
-        theme = "gruvbox";
+        lualine_z = [ ''" " .. os.date("%R")'' ];
       };
     };
   };
