9510b7414ced43cc7d9ae121a66cb368fd1d6ccd
confusedalex <alex@confusedalex.dev>
2024-10-17 08:17:55 +0200

    fix: lualine

--- config/ui/lualine.nix
+++ config/ui/lualine.nix
@@ -2,59 +2,12 @@
   plugins.lualine = {
     enable = true;
     settings = {
-      alwaysDivideMiddle = true;
-      globalstatus = true;
-      ignoreFocus = [ "neo-tree" ];
-      extensions = [ "fzf" ];
-      theme = "auto";
-      componentSeparators = {
-        left = "|";
-        right = "|";
-      };
-      sectionSeparators = {
-        left = "█"; # 
-        right = "█"; # 
-      };
-      sections = {
-        lualine_a = [ "mode" ];
-        lualine_b = [
-          "branch"
-          ""
-          "diff"
-          "diagnostics"
+      options = {
+        disabled_filetypes = [
+          "dashboard"
+          "alpha"
+          "ministarter"
         ];
-        lualine_c = [ "filename" ];
-        lualine_x = [ "filetype" ];
-        lualine_y = [ "progress" ];
-        settings = {
-          alwaysDivideMiddle = true;
-          globalstatus = true;
-          ignoreFocus = [ "neo-tree" ];
-          extensions = [ "fzf" ];
-          theme = "auto";
-          componentSeparators = {
-            left = "|";
-            right = "|";
-          };
-          sectionSeparators = {
-            left = "█"; # 
-            right = "█"; # 
-          };
-          sections = {
-            lualine_a = [ "mode" ];
-            lualine_b = [
-              "branch"
-              ""
-              "diff"
-              "diagnostics"
-            ];
-            lualine_c = [ "filename" ];
-            lualine_x = [ "filetype" ];
-            lualine_y = [ "progress" ];
-            lualine_z = [ ''" " .. os.date("%R")'' ];
-          };
-        };
-        lualine_z = [ ''" " .. os.date("%R")'' ];
       };
     };
   };
