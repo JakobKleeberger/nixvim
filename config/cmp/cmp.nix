142f83a633e6fe7e8ce2e48214f8623f144f497e
confusedalex <alex@confusedalex.dev>
2024-10-17 08:18:11 +0200

    fix: supertab and cmp-cmdline

--- config/cmp/cmp.nix
+++ config/cmp/cmp.nix
@@ -1,5 +1,6 @@
 {
   plugins = {
+    cmp-cmdline.enable = true;
     cmp = {
       enable = true;
       autoEnableSources = true;
@@ -52,4 +53,24 @@
       };
     };
   };
+  extraConfigLua = ''
+    luasnip = require("luasnip")
+    local cmp = require'cmp'
+
+    -- -- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
+    -- cmp.setup.cmdline({'/', "?" }, {
+    --   sources = {
+    --     { name = 'buffer' }
+    --   }
+    -- })
+    --
+    -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+    cmp.setup.cmdline(':', {
+      sources = cmp.config.sources({
+        { name = 'path' }
+      }, {
+      { name = 'cmdline' }
+      }),
+    })  
+  '';
 }
