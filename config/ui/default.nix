84ed75107b98fa0a2cb93e14a97441855006fc75
confusedalex <alex@confusedalex.dev>
2024-10-25 21:41:18 +0200

    feat: added render-markdown

--- config/ui/default.nix
+++ config/ui/default.nix
@@ -3,10 +3,12 @@
     ./bufferline.nix
     ./dressing.nix
     ./lualine.nix
+    ./markdown-render.nix
     ./mini-icons.nix
     ./mini-indentscope.nix
     ./noice.nix
     ./notify.nix
+    ./precognition.nix
     ./telescope.nix
   ];
 }
