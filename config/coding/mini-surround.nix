873f83e999c08eaa7d762c8779a6c873b67616a5
confusedalex <alex@confusedalex.dev>
2025-01-22 19:15:40 +0100

    fix: revert to default bindings

--- config/coding/mini-surround.nix
+++ config/coding/mini-surround.nix
@@ -1,17 +1,7 @@
 {
   plugins.mini = {
     modules = {
-      surround = {
-        mappings = {
-          add = "gsa";
-          delete = "gsd";
-          find = "gsf";
-          find_left = "gsF";
-          highlight = "gsh";
-          replace = "gsr";
-          update_n_lines = "gsn";
-        };
-      };
+      surround = {};
     };
   };
 }
