81c033fa360603370c12d7c706e337246cc65dae
confusedalex <alex@confusedalex.dev>
2025-01-22 19:05:01 +0100

    fix: fidget

--- config/lsp/fidget.nix
+++ config/lsp/fidget.nix
@@ -2,9 +2,10 @@
   plugins.fidget = {
     enable = true;
     settings = {
-      suppress_on_insert = true;
-      ignore_done_already = true;
-      poll_rate = 0.5;
+      progress = {
+        suppress_on_insert = true;
+        ignore_done_already = true;
+      };
     };
   };
 }
