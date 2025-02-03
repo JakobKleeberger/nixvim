8971987c3347e01b3b7db30c5b99b70923aa1cab
JakobKleeberger <kleeberger.jakob@aol.com>
2025-02-03 10:16:36 +0100

    temp fix for Neorg

--- config/utils/neorg.nix
+++ config/utils/neorg.nix
@@ -1,36 +1,38 @@
 {
-  plugins.neorg = {
-    enable = true;
-    telescopeIntegration.enable = true;
-
-    settings = {
-      load = {
-        "core.defaults" = { };
-        "core.completion" = { };
-        "core.concealer" = {
-          config = {
-            folds = true;
-          };
-        };
-        "core.dirman" = {
-          config = {
-            workspaces = {
-              home = "~/notes/home";
-            };
-            index = "index.norg";
-          };
-        };
-        "core.export" = {
-          config = {
-            export_dir = "~/notes";
-          };
-        };
-        "core.export.markdown" = { };
-        "core.latex.renderer" = { };
-        "core.presenter" = { };
-        "core.summary" = { };
-        "core.text-objects" = { };
-      };
-    };
-  };
+  plugins.neorg.enable = true;
+  # plugins.neorg = {
+  #   enable = true;
+  #   telescopeIntegration.enable = true;
+  #   autoLoad = true;
+  #
+  #   settings = {
+  #     load = {
+  #       "core.defaults" = { };
+  #       "core.completion" = { };
+  #       "core.concealer" = {
+  #         config = {
+  #           folds = true;
+  #         };
+  #       };
+  #       "core.dirman" = {
+  #         config = {
+  #           workspaces = {
+  #             home = "~/notes/home";
+  #           };
+  #           index = "index.norg";
+  #         };
+  #       };
+  #       "core.export" = {
+  #         config = {
+  #           export_dir = "~/notes";
+  #         };
+  #       };
+  #       "core.export.markdown" = { };
+  #       "core.latex.renderer" = { };
+  #       "core.presenter" = { };
+  #       "core.summary" = { };
+  #       "core.text-objects" = { };
+  #     };
+  #   };
+  # };
 }
